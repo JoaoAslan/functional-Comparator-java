8dff463da49892761969fd0726d135f4705d16d0
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 14:28:26 -0300

    Solution: Arrow Function in List.sort() (w/ Lambda One Line) 3

--- application/Program.java
+++ application/Program.java
@@ -15,10 +15,7 @@ public class Program {
         list.add(new Product("Notebook", 1200.0));
         list.add(new Product("Tablet", 450.0));
 
-        // FUNÇÃO ANONIMA (LAMBDA)
-        Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        list.sort(comp);
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
